92ba0ce1ecdd917d4cd0e0c68401d64c3cb906c0
L.Rain <bxy2997@sina.com>
2020-03-20 23:08:19 +0800

    React基础->拆分组件

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+      list: [
+        {
+          title: "React",
+          url: "https://facebook.github.io/react/",
+          author: "Jordan Walke",
+          num_comments: 3,
+          points: 4,
+          objectID: 0
+        },
+        {
+          title: "Redux",
+          url: "https://github.com/reactjs/redux",
+          author: "Dan Abramov, Andrew Clark",
+          num_comments: 2,
+          points: 5,
+          objectID: 1
+        }
+      ]
+    };
+  }
+  onDismiss = id => {
+    this.setState({
+      list:this.state.list.filter(x=>x.objectID!==id)
+    })
+  };
+  onSearchChange = event => {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Search
+            value={this.state.searchTerm}
+            onChange={this.onSearchChange}
+          />
+          <Table
+            list={this.state.list}
+            pattern={this.state.searchTerm}
+            onDismiss={this.onDismiss}
+          ></Table>
+        </header>
+      </div>
+    );
+  }
 }
-
+class Search extends Component {
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          value={this.props.value}
+          onChange={this.props.onChange}
+        ></input>
+      </form>
+    );
+  }
+}
+class Table extends Component {
+  render() {
+    console.log('in')
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list
+          .filter(x => x.title.toLowerCase().includes(pattern.toLowerCase()))
+          .map(item => {
+            return (
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                <span>
+                  <button
+                    onClick={() => onDismiss(item.objectID)}
+                    type="button"
+                  >
+                    Dismiss
+                  </button>
+                </span>
+              </div>
+            );
+          })}
+      </div>
+    );
+  }
+}
 export default App;
