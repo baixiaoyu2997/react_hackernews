a8df530e124ee7494a8a21820b790b175295b9d8
L.Rain <bxy2997@sina.com>
2020-03-22 17:41:40 +0800

    使用真实的API->错误处理

--- src/App.js
+++ src/App.js
@@ -15,17 +15,24 @@ class App extends Component {
     super(props);
 
     this.state = {
-      result: null,
-      searchTerm: "",
-      page: 0
+      results: null,
+      searchKey: "",
+      searchTerm: DEFAULT_QUERY,
+      error: null
     };
 
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
+  needsToSearchTopStories = searchTerm => {
+    return !this.state.results[searchTerm];
+  };
   onSearchSubmit = event => {
     const { searchTerm } = this.state;
-    this.fetchSearchTopStories(searchTerm);
+    this.setState({ searchKey: searchTerm });
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
     event.preventDefault();
   };
   onSearchChange(event) {
@@ -33,20 +40,26 @@ class App extends Component {
   }
 
   onDismiss(id) {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.result.hits.filter(isNotId);
+    const updatedHits = hits.filter(isNotId);
     this.setState({
-      result: {
-        ...this.state.result,
-        hits: updatedList
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
       }
     });
   }
   setSearchTopStories = result => {
     const { hits, page } = result;
-    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const { searchKey, results } = this.state;
+    const oldHits =
+      results && results[searchKey] ? results[searchKey].hits : [];
     const updatedHits = [...oldHits, ...hits];
-    this.setState({ result: { hits: updatedHits, page } });
+    this.setState({
+      results: { ...results, [searchKey]: { hits: updatedHits, page } }
+    });
   };
   fetchSearchTopStories = (searchTerm, page = 0) => {
     fetch(
@@ -54,14 +67,19 @@ class App extends Component {
     )
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(e => e);
+      .catch(e => this.setState({ error: e }));
   };
   componentDidMount() {
-    this.fetchSearchTopStories(this.state.searchTerm);
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopStories(searchTerm);
   }
   render() {
-    const { searchTerm, result, page } = this.state;
-    if (!result) return null;
+    const { searchTerm, results, searchKey, error } = this.state;
+    const page =
+      (results && results[searchKey] && results[searchKey].page) || 0;
+    const list =
+      (results && results[searchKey] && results[searchKey].hits) || [];
     return (
       <div className="page">
         <div className="interactions">
@@ -73,16 +91,14 @@ class App extends Component {
             搜索
           </Search>
         </div>
-        {result && (
-          <Table
-            list={result.hits}
-            pattern={searchTerm}
-            onDismiss={this.onDismiss}
-          />
+        {error ? (
+          <p>出错了</p>
+        ) : (
+          <Table list={list} onDismiss={this.onDismiss} />
         )}
         <div className="interactions">
           <Button
-            onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}
+            onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
           >
             More
           </Button>
@@ -99,7 +115,7 @@ const Search = ({ value, onChange, children, onSubmit }) => (
   </form>
 );
 
-const Table = ({ list, pattern, onDismiss }) => (
+const Table = ({ list, onDismiss }) => (
   <div className="table">
     {list.map(item => (
       <div key={item.objectID} className="table-row">
